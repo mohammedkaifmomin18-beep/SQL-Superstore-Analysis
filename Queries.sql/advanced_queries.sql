bff8e0e5a48ebece04084fdaf14bdf4fbbb835cd
Mohammed Kaif Momin <mohammedkaifmomin18@gmail.com>
2026-05-12 13:35:51 +0530

    Update advanced_queries.sql

--- Queries.sql/advanced_queries.sql
+++ Queries.sql/advanced_queries.sql
@@ -2,3 +2,10 @@ SELECT * FROM superstore LIMIT 10;
 
 -- Total rows
 SELECT COUNT(*) FROM superstore;
+
+-- Total sales & profit
+SELECT SUM(sales) AS total_sales, SUM(profit) AS total_profit
+FROM superstore;
+
+-- Unique customers
+SELECT COUNT(DISTINCT customer_id) FROM superstore;
